f95a33b9ed1f357fbf0a4c86deba68d72fcc4a0a
spiderasasen <spiderasasen@gmail.com>
2025-12-18 11:16:37 -0500

    adding the fake functions into the game. now to add some lore ones, but this will only be shown for this windows 11 feel

--- js/opening_window.js
+++ js/opening_window.js
@@ -1,3 +1,6 @@
 function opening_window(link){
     window.open(link, '_blank', "width=800, height=600");
 }
+function opening_files(link){
+    window.open(link, '_blank', "width=400, height=600");
+}
